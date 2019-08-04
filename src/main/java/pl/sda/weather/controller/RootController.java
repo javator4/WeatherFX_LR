25a3e4f1ce31c07d197eaf1a435c9301b2e10b5c
Switchu <remplak@gmail.com>
2019-08-04 12:38:17 +0200

    Update RootController.java

--- src/main/java/pl/sda/weather/controller/RootController.java
+++ src/main/java/pl/sda/weather/controller/RootController.java
@@ -2,12 +2,15 @@ package pl.sda.weather.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import pl.sda.WeatherService;
 
-public class RootController {
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class RootController implements Initializable {
     @FXML
     private TextField city;
     @FXML
@@ -18,9 +21,29 @@ public class RootController {
                     apiKEY = "c43efeb2af124a358ca81413191307";
     private WeatherService weatherService = new WeatherService(adress, apiKEY);
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("URUCHOMILEM OKNO");
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Info");
+        alert.setContentText("To jest moja aplikacja");
+        alert.show();
+
+        Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
+        alert1.setTitle("Potwierdzenie");
+        alert1.setContentText("Jesteś pewien?");
+        Optional<ButtonType> result = alert1.showAndWait();
+        if(result.get() == ButtonType.OK){
+            System.out.println("Użyszkodnik wybrał OK");
+        } else {
+            System.out.println("Użyszkodnik wybrał cancel");
+        }
+    }
 
     public void getWeather(ActionEvent actionEvent) {
         setTemperature(weatherService.getWeather(getCityInput()).getCurrent().getTemp_c());
+
     }
 
     private String getCityInput(){
